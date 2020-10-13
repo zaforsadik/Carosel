f816992d282bd119ff5aa6c58a2c958c4f4dfe62
zaforsadik <71747764+zaforsadik@users.noreply.github.com>
2020-10-14 03:15:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel({
+//         autoplay:true,
+//         autoplayTimeout:800,
+//         autoplayHoverPause:true,
+//         loop:true,
+//         margin:10,
+//         responsiveClass:true,
+        
+
+//     });
+//   });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:800,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.siam22 owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:800,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
